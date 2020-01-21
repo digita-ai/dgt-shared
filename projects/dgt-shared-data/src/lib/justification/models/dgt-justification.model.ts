47ac0f308b90abe85db40c2ceaff2cd0aac2f4e9
Wouter Alexander Janssens <wouter@MacBook-Pro-de-Wouter.local>
2020-01-21 12:45:36 +0100

    added icon and description to justification

--- projects/dgt-shared-data/src/lib/justification/models/dgt-justification.model.ts
+++ projects/dgt-shared-data/src/lib/justification/models/dgt-justification.model.ts
@@ -2,5 +2,7 @@ import { DGTLDField } from '../../linked-data/models/dgt-ld-field.model';
 import { DGTEntity } from '../../metadata/models/dgt-entity.model';
 
 export interface DGTJustification extends DGTEntity {
+    icon: string;
+    description: string;
     fields: DGTLDField[];
 }
