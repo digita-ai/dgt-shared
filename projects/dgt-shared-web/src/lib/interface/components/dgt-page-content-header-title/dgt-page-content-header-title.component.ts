ea3cf29f57c031df566ce8bd617220ce88286908
BelgianNoise <king.arthur360@gmail.com>
2020-03-27 16:41:40 +0100

    added page-content-header

---
+++ projects/dgt-shared-web/src/lib/interface/components/dgt-page-content-header-title/dgt-page-content-header-title.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dgt-page-content-header-title',
+  templateUrl: './dgt-page-content-header-title.component.html',
+  styleUrls: ['./dgt-page-content-header-title.component.scss']
+})
+export class DGTPageContentHeaderTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+
+}
---
+++ projects/dgt-shared-web/src/lib/interface/components/dgt-page-content-header/dgt-page-content-header.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dgt-page-content-header',
+  templateUrl: './dgt-page-content-header.component.html',
+  styleUrls: ['./dgt-page-content-header.component.scss']
+})
+export class DGTPageContentHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+
+}
