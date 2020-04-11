07961251925c0d3364d77b96b9db2a58e7433bd1
Wouter Alexander Janssens <wouter@MacBook-Pro-de-Wouter.local>
2020-04-11 09:17:45 +0200

    added argument checks to date to label service

--- projects/dgt-shared-utils/src/lib/date/services/dgt-date-to-label.service.ts
+++ projects/dgt-shared-utils/src/lib/date/services/dgt-date-to-label.service.ts
@@ -1,42 +1,51 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import moment from 'moment';
+import { DGTErrorArgument } from '../../errors/models/dgt-error-argument.model';
 
 @Injectable()
-export class DGTDateToLabelService{
+export class DGTDateToLabelService {
 
-    constructor() {}
+    constructor() { }
 
     public dateToReadableString(date: Date): string {
-        if (moment(date).isSame(moment(), 'day')){
+        if (!date) {
+            throw new DGTErrorArgument('date should be set.', date);
+        }
+
+        if (moment(date).isSame(moment(), 'day')) {
             return 'Today';
-        }else if (moment(date).isSame(moment().subtract(1, 'days'), 'day')){
+        } else if (moment(date).isSame(moment().subtract(1, 'days'), 'day')) {
             return 'Yesterday';
-        }else{
+        } else {
             return date.toDateString();
         }
     }
 
     public dateToTimeAgoString(date: Date): string {
+        if (!date) {
+            throw new DGTErrorArgument('date should be set.', date);
+        }
+
         const now = moment(new Date());
         const duration = moment.duration(now.diff(date));
         const minutes = Math.round(duration.asMinutes());
-        if ( minutes < 60 ) {
-            if ( minutes < 2 ) {
+        if (minutes < 60) {
+            if (minutes < 2) {
                 return 'Just Now';
             } else {
                 return minutes + ' minutes ago';
             }
         } else {
             const hours = Math.round(duration.asHours());
-            if ( hours < 24 ) {
+            if (hours < 24) {
                 return hours + (hours === 1 ? ' hour ago' : ' hours ago');
             } else {
                 const days = Math.round(duration.asDays());
-                if ( days < 31 ) {
+                if (days < 31) {
                     return days + (days === 1 ? ' day ago' : ' days ago');
                 } else {
                     const months = Math.round(duration.asMonths());
-                    if ( months < 12 ) {
+                    if (months < 12) {
                         return months + (months === 1 ? ' month ago' : ' months ago');
                     } else {
                         const years = Math.round(duration.asYears());
@@ -44,6 +53,6 @@ export class DGTDateToLabelService{
                     }
                 }
             }
-        }   
+        }
     }
 }
