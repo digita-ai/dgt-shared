670d6f56cc137afc095592801a8e664874e58dfd
lem-onade <taelemansst@gmail.com>
2020-04-16 13:21:06 +0200

    refactor: tidied up isSolidServer

--- projects/dgt-shared-connectors/src/lib/solid/validators/dgt-source-solid.validator.ts
+++ projects/dgt-shared-connectors/src/lib/solid/validators/dgt-source-solid.validator.ts
@@ -1,4 +1,4 @@
-import { DGTErrorArgument, DGTHttpService } from '@digita/dgt-shared-utils';
+import { DGTErrorArgument, DGTHttpService, DGTLoggerService } from '@digita/dgt-shared-utils';
 import { Injectable } from '@angular/core';
 import { HttpHeaders } from '@angular/common/http';
 
@@ -9,37 +9,46 @@ import { HttpHeaders } from '@angular/common/http';
 export class DGTSourceSolidValidator {
 
     constructor(
-        private http: DGTHttpService
+        private http: DGTHttpService,
+        private logger: DGTLoggerService
     ) { }
 
     /**
-     * Check if a solid server is running on an url
+     * Check if a solid server is running on the given url
      * @param url url of the solid server to test
      * @returns true if the specified url is a solid server, false if not
      * @throws DGTErrorArgument if url is invalid or server does not return 200
      */
     public isSolidServer(url: string): boolean {
         if (!url) {
-            throw new DGTErrorArgument('url is null or undefined', url);
+            throw new DGTErrorArgument('Passed url is null or undefined', url);
         }
-        // Check if url is valid
-        // Copyright (c) 2010-2018 Diego Perini (http://www.iport.it)
+        // Check headers for Link
+        this.http.head(url).subscribe(res => {
+            const headers = res.headers;
+            if (res.status !== 200) {
+                this.logger.debug(DGTSourceSolidValidator.name, 'Status was not 200', res.status);
+                return false;
+            }
+            if (!headers.has('link')) {
+                this.logger.debug(DGTSourceSolidValidator.name, 'Headers did not contain Link', headers);
+                return false;
+            }
+            if (headers.get('link') !== '<.acl>; rel="acl", <.meta>; rel="describedBy", <http://www.w3.org/ns/ldp#Resource>; rel="type"') {
+                this.logger.debug(DGTSourceSolidValidator.name, 'Link header value did not match', headers.get('link'));
+                return false;
+            }
+        });
+        // Check if .well-known/solid-configuration exists on server
+        this.http.get(url + '/.well-known/openid-configuration').subscribe(res => {
+            if (res.status !== 200) {
+                this.logger.debug(DGTSourceSolidValidator.name, 'Status was not 200', res.status);
+                return false;
+            }
+        });
 
-        const regex = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
-
-        if (regex.test(url)) {
-            // Check if url has solid-server header
-            let headers: HttpHeaders;
-            this.http.head(url).subscribe(res => {
-                if (res.status !== 200) {
-                    return false;
-                }
-                headers = res.headers;
-                return headers.has('X-Powered-By') && /solid-server\/.*/.test(headers.get('X-Powered-By'));
-            });
-
-        } else {
-            return false;
-        }
+        this.logger.debug(DGTSourceSolidValidator.name, 'URL has a solid server', url);
+        // When the url passes all of the previous checks, it is officially granted 'solid-server' status and a small applause
+        return true;
     }
 }
