4d2dd28db2d6a156fc00ac53ca3232f17434720f
lem-onade <taelemansst@gmail.com>
2020-04-17 11:11:29 +0200

    fix: validator did not work as expected

--- projects/dgt-shared-connectors/src/lib/solid/validators/dgt-source-solid.validator.ts
+++ projects/dgt-shared-connectors/src/lib/solid/validators/dgt-source-solid.validator.ts
@@ -1,5 +1,7 @@
 import { DGTErrorArgument, DGTHttpService, DGTLoggerService } from '@digita/dgt-shared-utils';
 import { Injectable } from '@angular/core';
+import { Observable, of, pipe } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 
 /**
  * Validator for all things solid-related
@@ -17,43 +19,76 @@ export class DGTSourceSolidValidator {
      * @param url url to test
      * @returns true if the specified url is a solid server, false if not
      */
-    public isSolidServer(url: string): boolean {
+    public isSolidServer(url: string): Observable<boolean> {
         if (!url) {
-            throw new DGTErrorArgument('Passed url is null or undefined', url);
+            this.logger.debug(DGTSourceSolidValidator.name, 'URL was undefined or null', url);
+            return of(false);
         }
         // Test if url is valid
         // Copyright (c) 2010-2018 Diego Perini (http://www.iport.it)
         const reg = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
         if (!reg.test(url)) {
             this.logger.debug(DGTSourceSolidValidator.name, 'URL was not valid', url);
-            return false;
+            return of(false);
+        } else {
+            // Check headers for Link
+            return this.http.head(url).pipe(
+                map(res => {
+                        const headers = res.headers;
+                        if (res.status !== 200) {
+                            this.logger.debug(DGTSourceSolidValidator.name, 'Status was not 200', res.status);
+                            return false;
+                        } else if (!headers.has('link')) {
+                            this.logger.debug(DGTSourceSolidValidator.name, 'Headers did not contain Link', headers);
+                            return false;
+                        } else if (headers.get('link') !== '<.acl>; rel="acl", <.meta>; rel="describedBy", <http://www.w3.org/ns/ldp#Resource>; rel="type"') {
+                            this.logger.debug(DGTSourceSolidValidator.name, 'Link header value did not match', headers.get('link'));
+                            return false;
+                        } else {
+                            return true;
+                        }
+                    })
+            )
+            &&
+            // Check if /.well-known/openid-configuration exists on server
+            this.http.get(url + '/.well-known/openid-configuration').pipe(
+                map(getRes => {
+                if (getRes.status !== 200) {
+                    this.logger.debug(DGTSourceSolidValidator.name, 'Status was not 200', getRes.status);
+                    return false;
+                } else {
+                    this.logger.debug(DGTSourceSolidValidator.name, 'URL has a solid server', url);
+                    // When the url passes all of the previous checks, it is granted 'solid-server' status and awarded a small applause
+                    return true;
+                }
+                })
+            );
         }
-        // Check headers for Link
-        this.http.head(url).subscribe(res => {
-            const headers = res.headers;
-            if (res.status !== 200) {
-                this.logger.debug(DGTSourceSolidValidator.name, 'Status was not 200', res.status);
-                return false;
-            }
-            if (!headers.has('link')) {
-                this.logger.debug(DGTSourceSolidValidator.name, 'Headers did not contain Link', headers);
-                return false;
-            }
-            if (headers.get('link') !== '<.acl>; rel="acl", <.meta>; rel="describedBy", <http://www.w3.org/ns/ldp#Resource>; rel="type"') {
-                this.logger.debug(DGTSourceSolidValidator.name, 'Link header value did not match', headers.get('link'));
-                return false;
-            }
-        });
-        // Check if .well-known/solid-configuration exists on server
-        this.http.get(url + '/.well-known/openid-configuration').subscribe(res => {
-            if (res.status !== 200) {
-                this.logger.debug(DGTSourceSolidValidator.name, 'Status was not 200', res.status);
-                return false;
-            }
-        });
-
-        this.logger.debug(DGTSourceSolidValidator.name, 'URL has a solid server', url);
-        // When the url passes all of the previous checks, it is officially granted 'solid-server' status and awarded a small applause
-        return true;
     }
 }
+
+// {
+//     const headers = res.headers;
+//     if (res.status !== 200) {
+//         this.logger.debug(DGTSourceSolidValidator.name, 'Status was not 200', res.status);
+//         return of(false);
+//     } else if (!headers.has('link')) {
+//         this.logger.debug(DGTSourceSolidValidator.name, 'Headers did not contain Link', headers);
+//         return of(false);
+//     } else if (headers.get('link') !== '<.acl>; rel="acl", <.meta>; rel="describedBy", <http://www.w3.org/ns/ldp#Resource>; rel="type"') {
+//         this.logger.debug(DGTSourceSolidValidator.name, 'Link header value did not match', headers.get('link'));
+//         return of(false);
+//     } else {
+//         // Check if .well-known/solid-configuration exists on server
+//         this.http.get(url + '/.well-known/openid-configuration').subscribe(getRes => {
+//             if (getRes.status !== 200) {
+//                 this.logger.debug(DGTSourceSolidValidator.name, 'Status was not 200', getRes.status);
+//                 return of(false);
+//             } else {
+//                 this.logger.debug(DGTSourceSolidValidator.name, 'URL has a solid server', url);
+//                 // When the url passes all of the previous checks, it is granted 'solid-server' status and awarded a small applause
+//                 return of(true);
+//             }
+//         });
+//     }
+// }
