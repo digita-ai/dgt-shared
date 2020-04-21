159d804db4ed0ee97bc3f8ce02e053e8f8af9dc2
Wouter Alexander Janssens <wouter@MacBook-Pro-de-Wouter.local>
2020-04-21 10:40:05 +0200

    Indicate selected connection in rail

--- projects/dgt-shared-web/src/lib/interface/components/dgt-page-rail-item/dgt-page-rail-item.component.ts
+++ projects/dgt-shared-web/src/lib/interface/components/dgt-page-rail-item/dgt-page-rail-item.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'dgt-page-rail-item',
   templateUrl: './dgt-page-rail-item.component.html',
-  styleUrls: ['./dgt-page-rail-item.component.scss']
+  styleUrls: ['./dgt-page-rail-item.component.scss'],
 })
 export class DGTPageRailItemComponent implements OnInit {
+
+  @Input() @HostBinding('class.selected') selected = false;
 
   constructor() { }
 
