8d973a3231cc5fcc318f77f5ff60ade6f8c90367
lem-onade <taelemansst@gmail.com>
2020-04-27 10:04:26 +0200

    refactor: moved isSolidServer to connector

--- projects/dgt-shared-connectors/src/lib/solid/connectors/dgt-source-solid.connector.ts
+++ projects/dgt-shared-connectors/src/lib/solid/connectors/dgt-source-solid.connector.ts
@@ -295,7 +295,7 @@ export class DGTSourceSolidConnector implements DGTSourceConnector<DGTSourceSoli
      * @param username The username you want to check for existance
      * @returns True if the username is available, false if not
      */
-    public checkIfUsernameAvailable(source: DGTSourceSolid, username: string): Observable<boolean> {
+    public isAvailableUsername(source: DGTSourceSolid, username: string): Observable<boolean> {
         this.logger.debug(DGTSourceSolidConnector.name, 'Checking if username exists on solid server', { source, username });
 
         const sourceuri = source.configuration.issuer;
@@ -305,6 +305,58 @@ export class DGTSourceSolidConnector implements DGTSourceConnector<DGTSourceSoli
             tap(response => this.logger.debug(DGTSourceSolidConnector.name, 'Received response', { response })),
             map(response => (response.status === 404) )
         );
+    }
+
+    /**
+     * Check if a solid server is running on the given url
+     * @param url url to test
+     * @returns true if the specified url is a solid server, false if not
+     */
+    public isSolidServer(url: string): Observable<boolean> {
+        if (!url) {
+            this.logger.debug(DGTSourceSolidConnector.name, 'URL was undefined or null', url);
+            return of(false);
+        }
+        // Test if url is valid
+        // Copyright (c) 2010-2018 Diego Perini (http://www.iport.it)
+        const reg = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+        if (!reg.test(url)) {
+            this.logger.debug(DGTSourceSolidConnector.name, 'URL was not valid', url);
+            return of(false);
+        } else {
+            // Check headers for Link
+            return this.http.head(url).pipe(
+                map(res => {
+                        const headers = res.headers;
+                        if (res.status !== 200) {
+                            this.logger.debug(DGTSourceSolidConnector.name, 'Status was not 200', res.status);
+                            return false;
+                        } else if (!headers.has('link')) {
+                            this.logger.debug(DGTSourceSolidConnector.name, 'Headers did not contain Link', headers);
+                            return false;
+                        } else if (headers.get('link') !== '<.acl>; rel="acl", <.meta>; rel="describedBy", <http://www.w3.org/ns/ldp#Resource>; rel="type"') {
+                            this.logger.debug(DGTSourceSolidConnector.name, 'Link header value did not match', headers.get('link'));
+                            return false;
+                        } else {
+                            return true;
+                        }
+                    })
+            )
+            &&
+            // Check if /.well-known/openid-configuration exists on server
+            this.http.get(url + '/.well-known/openid-configuration').pipe(
+                map(getRes => {
+                if (getRes.status !== 200) {
+                    this.logger.debug(DGTSourceSolidConnector.name, 'Status was not 200', getRes.status);
+                    return false;
+                } else {
+                    this.logger.debug(DGTSourceSolidConnector.name, 'URL has a solid server', url);
+                    // When the url passes all of the previous checks, it is granted 'solid-server' status and awarded a small applause
+                    return true;
+                }
+                })
+            );
+        }
     }
 
     private generateSparqlUpdate(entities: DGTLDEntity[], updateType: 'insert' | 'delete'): string {
--- projects/dgt-shared-connectors/src/public-api.ts
+++ projects/dgt-shared-connectors/src/public-api.ts
@@ -5,7 +5,6 @@
 export { DGTSourceMSSQLConnector } from './lib/mssql/connectors/dgt-source-mssql.connector';
 export { DGTSourceMSSQLConfiguration } from './lib/mssql/models/dgt-source-mssql-configuration.model';
 export { DGTSourceSolidConnector } from './lib/solid/connectors/dgt-source-solid.connector';
-export { DGTSourceSolidValidator } from './lib/solid/validators/dgt-source-solid.validator';
 export { DGTSourceSolidToken } from './lib/solid/models/dgt-source-solid-token.model';
 export { DGTSourceGravatarConfiguration } from './lib/gravatar/models/dgt-source-gravatar-configuration.model';
 export { DGTSourceGravatarResponse } from './lib/gravatar/models/dgt-source-gravatar-response.model';
