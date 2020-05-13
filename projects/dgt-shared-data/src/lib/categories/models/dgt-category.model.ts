ade6815942db976add0e72190348497182c51242
Wouter Alexander Janssens <wouter@MacBook-Pro-van-Wouter.local>
2020-05-13 22:28:43 +0200

    renamed category filter file

--- projects/dgt-shared-data/src/lib/categories/models/dgt-category.model.ts
+++ projects/dgt-shared-data/src/lib/categories/models/dgt-category.model.ts
@@ -1,5 +1,5 @@
 import { DGTEntity } from '../../metadata/models/dgt-entity.model';
-import { DGTCategoryFilter } from './dgt-category-field.model';
+import { DGTCategoryFilter } from './dgt-category-filter.model';
 
 export interface DGTCategory extends DGTEntity {
     icon: string;
