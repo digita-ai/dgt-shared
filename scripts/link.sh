efbda0a90be0f4185d3f9a4b79ec868255f74924
sander <sander.claes@amplexor.com>
2020-06-10 16:56:12 +0200

    Add linking script

---
+++ scripts/link.sh
@@ -0,0 +1,17 @@
+#bin/bash
+cd ../projects/dgt-shared-test;
+npm link;
+cd ../dgt-shared-utils/
+npm link;
+npm link ../dgt-shared-test;
+cd ../dgt-shared-data/;
+npm link;
+npm link ../dgt-shared-test;
+cd ../dgt-shared-connectors/;
+npm link;
+npm link ../dgt-shared-test;
+cd ../dgt-shared-web/;
+npm link;
+npm link ../dgt-shared-test;
+cd ../../scripts;
+./build.sh;
