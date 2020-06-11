6d286c4b1f624e463dbf963b193fcd84ba42b615
sander <sander.claes@amplexor.com>
2020-06-11 13:31:56 +0200

    update linking script

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,16 +1,16 @@
 #bin/bash
 cd ../projects/dgt-shared-test;
 npm link;
-cd ../dgt-shared-utils/
+cd ../dgt-shared-utils
 npm link;
 npm link ../dgt-shared-test;
-cd ../dgt-shared-data/;
+cd ../dgt-shared-data;
 npm link;
 npm link ../dgt-shared-test;
-cd ../dgt-shared-connectors/;
+cd ../dgt-shared-connectors;
 npm link;
 npm link ../dgt-shared-test;
-cd ../dgt-shared-web/;
+cd ../dgt-shared-web;
 npm link;
 npm link ../dgt-shared-test;
 cd ../../scripts;
