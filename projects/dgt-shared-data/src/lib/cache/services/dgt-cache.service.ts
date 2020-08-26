355973bfe373d58701305027b1d1a0fbf205c91e
Sander Claes <sander@digita.ai>
2020-08-26 17:09:39 +0200

    develop DGTLDService first implementation

--- projects/dgt-shared-data/src/lib/cache/services/dgt-cache.service.ts
+++ projects/dgt-shared-data/src/lib/cache/services/dgt-cache.service.ts
@@ -1,16 +1,22 @@
 import { Observable, of, forkJoin } from 'rxjs';
 import { DGTLoggerService } from '@digita/dgt-shared-utils';
-import { switchMap, map, tap } from 'rxjs/operators';
+import { switchMap, map, tap, mergeMap } from 'rxjs/operators';
 import { DGTDataService } from '../../metadata/services/dgt-data.service';
 import { DGTLDTriple } from '../../linked-data/models/dgt-ld-triple.model';
 import { DGTQuery } from '../../metadata/models/dgt-query.model';
 import { Injectable } from '@angular/core';
 import * as _ from 'lodash';
 import { DGTExchange } from '../../holder/models/dgt-holder-exchange.model';
+import { DGTLDFilter } from '../../linked-data/models/dgt-ld-filter.model';
+import { DGTLDTransformer } from '../../linked-data/models/dgt-ld-transformer.model';
+import { DGTLDFilterService } from '@digita/dgt-shared-data/public-api';
+import { DGTLDFilterType } from '../../linked-data/models/dgt-ld-filter-type.model';
+import { DGTLDFilterExchange } from '../../linked-data/models/dgt-ld-filter-exchange.model';
 
 @Injectable()
 export class DGTCacheService {
-    constructor(private data: DGTDataService, private logger: DGTLoggerService) { }
+    private cache: Observable<DGTLDTriple[]>;
+    constructor(private data: DGTDataService, private logger: DGTLoggerService, private filterService: DGTLDFilterService) { }
 
     public getValuesForExchange(exchange: DGTExchange): Observable<DGTLDTriple[]> {
         this.logger.debug(DGTCacheService.name, 'Retrieving values from cache for exchange', { exchange });
@@ -52,5 +58,30 @@ export class DGTCacheService {
                 tap(data => this.logger.debug(DGTCacheService.name, 'Removed old values, ready to store new ones', data)),
                 switchMap(data => this.data.createEntities<DGTLDTriple>('value', data.values)),
             );
+    }
+
+    public query<T>(filter: DGTLDFilter, transformer: DGTLDTransformer<T>): Observable<DGTLDTriple[]> {
+        if (this.cache) {
+            return this.cache.pipe(mergeMap(tripleArray => {
+                return this.filterService.run([filter], tripleArray);
+            }));
+        } else {
+            this.cache = this.getAllValues();
+            return this.query(filter, transformer);
+        }
+    }
+
+    private getAllValues(): Observable<DGTLDTriple[]> {
+        return this.data.getEntities<DGTExchange>('exchange', null).pipe(
+            map(exchanges => ({
+                type: DGTLDFilterType.EXCHANGE,
+                exchanges
+            })),
+            mergeMap(filterExchange =>
+                this.data.getEntities<DGTLDTriple>('value', null).pipe(
+                    switchMap(triples => this.filterService.run([filterExchange], triples))
+                )
+            )
+        );
     }
 }
