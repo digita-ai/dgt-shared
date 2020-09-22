84562ec3d5786e14309b980cd3dd9567cdd28c9d
Wouter Alexander Janssens <wouter@MacBook-Pro-van-Wouter.local>
2020-09-22 10:29:29 +0200

    added publish script

---
+++ scripts/publish.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+npm publish dist/dgt-shared-test
+npm publish dist/dgt-shared-utils
+npm publish dist/dgt-shared-data
+npm publish dist/dgt-shared-web
+npm publish dist/dgt-shared-connectors
