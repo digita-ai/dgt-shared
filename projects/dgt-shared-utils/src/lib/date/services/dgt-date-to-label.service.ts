c2dfceb48aa9e0fb4638952977fd8947b8cc6a3c
Arthur Joppart <arthur@digita.ai>
2020-09-25 11:07:40 +0200

    Revert "translation for dateService"
    This reverts commit d7bd5c11adcfb9a5f13100e8ae8c8711d21775d3.

--- projects/dgt-shared-utils/src/lib/date/services/dgt-date-to-label.service.ts
+++ projects/dgt-shared-utils/src/lib/date/services/dgt-date-to-label.service.ts
@@ -1,31 +1,27 @@
 import { Injectable } from '@angular/core';
 import moment from 'moment';
 import { DGTErrorArgument } from '../../errors/models/dgt-error-argument.model';
-import { TranslateService } from '@ngx-translate/core';
-import { Observable, of } from 'rxjs';
 
 @Injectable()
 export class DGTDateToLabelService {
 
-    constructor(
-        private translate: TranslateService
-    ) { }
+    constructor() { }
 
-    public dateToReadableString(date: Date): Observable<string> {
+    public dateToReadableString(date: Date): string {
         if (!date) {
             throw new DGTErrorArgument('date should be set.', date);
         }
 
         if (moment(date).isSame(moment(), 'day')) {
-            return this.translate.get('common.date.today');
+            return 'Today';
         } else if (moment(date).isSame(moment().subtract(1, 'days'), 'day')) {
-            return this.translate.get('common.date.yesterday');
+            return 'Yesterday';
         } else {
-            return of(date.toDateString());
+            return date.toDateString();
         }
     }
 
-    public dateToTimeAgoString(date: Date): Observable<string> {
+    public dateToTimeAgoString(date: Date): string {
         if (!date) {
             throw new DGTErrorArgument('date should be set.', date);
         }
@@ -35,29 +31,25 @@ export class DGTDateToLabelService {
         const minutes = Math.round(duration.asMinutes());
         if (minutes < 60) {
             if (minutes < 2) {
-                return this.translate.get('common.date.justNow');
+                return 'Just Now';
             } else {
-                return this.translate.get('common.date.minutesAgo', {minutes} );
+                return minutes + ' minutes ago';
             }
         } else {
             const hours = Math.round(duration.asHours());
             if (hours < 24) {
-                return hours === 1 ? this.translate.get('common.date.hourAgo', { hours } ) :
-                    this.translate.get('common.date.hoursAgo', { hours });
+                return hours + (hours === 1 ? ' hour ago' : ' hours ago');
             } else {
                 const days = Math.round(duration.asDays());
                 if (days < 31) {
-                    return days === 1 ? this.translate.get('common.date.dayAgo', { days } ) :
-                        this.translate.get('common.date.daysAgo', { days });
+                    return days + (days === 1 ? ' day ago' : ' days ago');
                 } else {
                     const months = Math.round(duration.asMonths());
                     if (months < 12) {
-                        return months === 1 ? this.translate.get('common.date.monthAgo', { months } ) :
-                            this.translate.get('common.date.monthsAgo', { months });
+                        return months + (months === 1 ? ' month ago' : ' months ago');
                     } else {
                         const years = Math.round(duration.asYears());
-                        return years === 1 ? this.translate.get('common.date.yearAgo', { years } ) :
-                            this.translate.get('common.date.yearsAgo', { years });
+                        return years + (years === 1 ? ' year ago' : ' years ago');
                     }
                 }
             }
