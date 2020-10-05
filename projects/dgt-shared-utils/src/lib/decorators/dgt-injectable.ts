0bc1a16806a3f9bb59fa38e24d0721a4a7a9816f
stijn-taelemans <stijn@digita.ai>
2020-10-05 15:32:45 +0200

    fixed dgtinjectable

--- projects/dgt-shared-utils/src/lib/decorators/dgt-injectable.ts
+++ projects/dgt-shared-utils/src/lib/decorators/dgt-injectable.ts
@@ -1,24 +1,24 @@
 /** Used to store require object */
-let projectType = null;
+let injectableImport = null;
+
 /**
  * This is a wrapper decorator that can be used in both Angular and nestjs projects
  * It will determine between Angular and nestjs' @DGTInjectable() decorator
  * So that services written in Angular projects can be used in nestjs projects and vice-versa
  */
 export function DGTInjectable() {
-    if (!projectType) {
+    if (!injectableImport) {
         const displayDate: string = new Date().toLocaleTimeString();
         console.log(`[${displayDate} DGTInjectable] Figuring out project type`);
         try {
-            projectType = require('@nestjs/common');
-            console.log(`[${displayDate} DGTInjectable] Type: Nestjs project`);
+            injectableImport = '@nestjs/common';
+            console.log(`[${displayDate} DGTInjectable] Project type: nestjs`);
+            return require(injectableImport).Injectable();
         } catch (e) {
-            if (e.code !== 'MODULE_NOT_FOUND') {
-                throw e;
-            }
-            projectType = require('@angular/core');
-            console.log(`[${displayDate} DGTInjectable] Type: Angular project`);
+            injectableImport = '@angular/core';
+            console.log(`[${injectableImport} DGTInjectable] Project type: angular`);
+            return require(injectableImport).Injectable();
         }
     }
-    return projectType.DGTInjectable();
+    return require(injectableImport).Injectable();
 }
