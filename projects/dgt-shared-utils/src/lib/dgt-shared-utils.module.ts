9f4408d8acbb9e553548b5b0c2d44541945b5650
Wouter Janssens <wouter@digita.ai>
2020-10-11 14:50:19 +0200

    renamed connectivity file

--- projects/dgt-shared-utils/src/lib/dgt-shared-utils.module.ts
+++ projects/dgt-shared-utils/src/lib/dgt-shared-utils.module.ts
@@ -2,7 +2,7 @@ import { NgModule, Type } from '@angular/core';
 import { DGTLoggerService } from './logging/services/dgt-logger.service';
 import { DGTErrorService } from './errors/services/dgt-error.service';
 import { DGTPlatformService } from './platform/services/dgt-platform.service';
-import { DGTConnectivityService } from './connection/services/dgt-connection.service';
+import { DGTConnectivityService } from './connectivity/services/dgt-connectivity.service';
 import { DGTHttpService } from './http/services/dgt-http.service';
 import { DGTHttpAngularService } from './http/services/dgt-http-angular.service';
 import { DGTCryptoBrowserService } from './crypto/services/dgt-crypto-browser.service';
