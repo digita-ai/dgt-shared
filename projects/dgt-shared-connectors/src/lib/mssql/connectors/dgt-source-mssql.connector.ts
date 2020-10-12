20aa23b4ec7beba72a9d5214d7dc5ec42b487f4c
Stijn Taelemans <stijn@digita.ai>
2020-10-12 12:01:57 +0200

    implemented update

--- projects/dgt-shared-connectors/src/lib/mssql/connectors/dgt-source-mssql.connector.ts
+++ projects/dgt-shared-connectors/src/lib/mssql/connectors/dgt-source-mssql.connector.ts
@@ -89,7 +89,28 @@ export class DGTSourceMSSQLConnector extends DGTSourceConnector<DGTSourceMSSQLCo
     }
 
     public update<R extends DGTLDResource>(domainEntities: { original: R, updated: R }[], connection: DGTConnection<DGTConnectionMSSQLConfiguration>, source: DGTSource<DGTSourceMSSQLConfiguration>, transformer: DGTLDTransformer<R>): Observable<R[]> {
-        throw new DGTErrorNotImplemented();
+        const config = this.extractConfig(source);
+        this.logger.debug(DGTSourceMSSQLConnector.name, 'Starting DELETE, creating connection pool', {
+            source
+        });
+
+        const pool = this.getPool(config);
+        this.logger.debug(DGTSourceMSSQLConnector.name, 'Created connection pool', { pool });
+
+        return of({ pool, config })
+            .pipe(
+                switchMap(data => from(data.pool.connect())
+                    .pipe(map(newPool => ({ newPool, ...data })))
+                ),
+                tap(data => this.logger.debug(DGTSourceMSSQLConnector.name, 'Connected to pool', { data })),
+                switchMap(data => {
+                    const columns = `name = ${domainEntities[0].updated}, points = '124`;
+                    return from(data.pool.request().query(source.configuration.commands.update(connection.configuration.personId, columns)))
+                    .pipe(map(result => ({ result, ...data })))
+                }),
+                tap(data => this.logger.debug(DGTSourceMSSQLConnector.name, 'Finished DELETE', { data })),
+                map(data => domainEntities.map(entity => entity.updated)),
+            );
     }
 
     public delete<R extends DGTLDResource>(domainEntities: R[], connection: DGTConnection<DGTConnectionMSSQLConfiguration>, source: DGTSource<DGTSourceMSSQLConfiguration>, transformer: DGTLDTransformer<R>): Observable<R[]> {
