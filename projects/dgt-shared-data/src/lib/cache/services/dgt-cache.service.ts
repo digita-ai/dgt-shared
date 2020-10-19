84cc108d22fa7614d13f6cde4d22e36f0ff070e0
Sander Claes <sander@digita.ai>
2020-10-19 10:51:43 +0200

    split cache service

--- projects/dgt-shared-data/src/lib/cache/services/dgt-cache.service.ts
+++ projects/dgt-shared-data/src/lib/cache/services/dgt-cache.service.ts
@@ -1,69 +1,24 @@
-import { Observable, of, forkJoin } from 'rxjs';
-import { DGTErrorArgument, DGTInjectable, DGTLoggerService } from '@digita-ai/dgt-shared-utils';
-import { switchMap, map, tap, mergeMap } from 'rxjs/operators';
+import { DGTInjectable } from '@digita-ai/dgt-shared-utils';
+import { Observable } from 'rxjs';
 import { DGTLDTriple } from '../../linked-data/models/dgt-ld-triple.model';
-import { DGTQuery } from '../../metadata/models/dgt-query.model';
-
 import * as _ from 'lodash';
-import { DGTExchange } from '../../holder/models/dgt-holder-exchange.model';
-import { DGTLDFilter } from '../../linked-data/models/dgt-ld-filter.model';
 import { DGTLDTransformer } from '../../linked-data/models/dgt-ld-transformer.model';
-import { DGTLDFilterService } from '../../linked-data/services/dgt-ld-filter.service';
-import { DGTQueryService } from '../../metadata/services/dgt-query.service';
+import { DGTConnection } from '../../connection/models/dgt-connection.model';
+import { DGTQuery } from '../../metadata/models/dgt-query.model';
+import { DGTLDFilter } from '../../linked-data/models/dgt-ld-filter.model';
+import { DGTLDResource } from '../../linked-data/models/dgt-ld-resource.model';
+import { DGTConnectionSolidConfiguration } from '../../connection/models/dgt-connection-solid-configuration.model';
+import { DGTSource } from '../../source/models/dgt-source.model';
+import { DGTSourceSolidConfiguration } from '../../source/models/dgt-source-solid-configuration.model';
 
 @DGTInjectable()
-export class DGTCacheService {
-
+export abstract class DGTCacheService {
     public cache: DGTLDTriple[];
+    public abstract remove(query: DGTQuery): Observable<any>
+    public abstract query<T>(filter: DGTLDFilter, transformer: DGTLDTransformer<T>): Observable<any>
+    public abstract delete<R extends DGTLDResource>(domainEntities: R[], connection: DGTConnection<DGTConnectionSolidConfiguration>, source: DGTSource<DGTSourceSolidConfiguration>, transformer: DGTLDTransformer<R>): Observable<R[]>
+    
+    //TODO check if we can remove the connection
+    public abstract save<T>(transformer: DGTLDTransformer<T>, toBeSaved: T[], connection: DGTConnection<any>): Observable<DGTLDTriple[] | T[]>
 
-    constructor(private logger: DGTLoggerService, private filterService: DGTLDFilterService, private queries: DGTQueryService) { 
-        this.cache = [];
-    }
-
-    public getValuesForExchange(exchange: DGTExchange): Observable<DGTLDTriple[]> {
-        this.logger.debug(DGTCacheService.name, 'Retrieving values from cache for exchange', { exchange });
-
-        if (!exchange) {
-            throw new DGTErrorArgument('Argument exchange should be set.', exchange);
-        }
-
-        return of(this.cache.filter(triple => triple.exchange === exchange.id));
-    }
-
-    public remove(query: DGTQuery): Observable<any> {
-        this.logger.debug(DGTCacheService.name, 'Removing values from cache', { query });
-
-        if (!query) {
-            throw new DGTErrorArgument('Argument query should be set.', query);
-        }
-
-        this.cache = this.queries.execute(this.cache, query);
-
-        return of(this.cache);
-    }
-
-    public storeForExchange(exchange: DGTExchange, values: DGTLDTriple[]): Observable<DGTLDTriple[]> {
-        this.logger.debug(DGTCacheService.name, 'Storing values for exchange to cache', { exchange, values });
-
-        return of({ values, exchange })
-            .pipe(
-                switchMap(data => this.remove({ conditions: [{ field: 'exchange', operator: '==', value: data.exchange.id }] })
-                    .pipe(map(removal => data))),
-                tap(data => this.logger.debug(DGTCacheService.name, 'Removed old values, ready to store new ones', data)),
-                tap(data => this.cache = data.values),
-                map(data => data.values),
-            );
-    }
-
-    public query<T>(filter: DGTLDFilter, transformer: DGTLDTransformer<T>): Observable<DGTLDTriple[] | T[]> {
-        return of(this.cache).pipe(mergeMap(tripleArray => {
-            let res;
-            if (filter) {
-                res = this.filterService.run(filter, tripleArray);
-            } else {
-                res = of(tripleArray);
-            }
-            return transformer ? transformer.toDomain(res) : res;
-        }));
-    }
 }
