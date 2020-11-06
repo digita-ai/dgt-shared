2ef53f4cfcafb3eb1005474d3160cc89934531b1
Wouter Janssens <wouter@digita.ai>
2020-11-06 07:14:10 +0100

    refactor: removed DGTEntity

--- projects/dgt-shared-web/src/lib/security/models/dgt-user.model.ts
+++
@@ -1,8 +0,0 @@
-import { DGTI8NEntity } from '../../i8n/models/dgt-i8n-entity.model';
-
-export interface DGTUser extends DGTI8NEntity {
-    id?: string;
-    emailValidated: boolean;
-    phone: string;
-    email: string;
-}
