2165f5809f4d2597df4ec643e01440b27508129b
Stijn Taelemans <stijn@digita.ai>
2020-11-09 11:40:26 +0100

    feat: created purpose cache service

---
+++ projects/dgt-shared-data/src/lib/purpose/services/dgt-purpose-cache.service.ts
@@ -0,0 +1,72 @@
+import { DGTErrorArgument, DGTInjectable, DGTLoggerService } from '@digita-ai/dgt-shared-utils';
+import * as _ from 'lodash';
+import { Observable, of } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { v4 } from 'uuid';
+import { DGTCacheService } from '../../cache/services/dgt-cache.service';
+import { DGTLDFilter } from '../../linked-data/models/dgt-ld-filter.model';
+import { DGTPurpose } from '../models/dgt-purpose.model';
+import { DGTPurposeTransformerService } from './dgt-purpose-transformer.service';
+import { DGTPurposeService } from './dgt-purpose.service';
+
+@DGTInjectable()
+export class DGTPurposeCacheService extends DGTPurposeService {
+
+    constructor(
+        private logger: DGTLoggerService,
+        private cache: DGTCacheService,
+        private transformer: DGTPurposeTransformerService
+    ) {
+        super();
+    }
+
+    public get(uri: string): Observable<DGTPurpose> {
+        this.logger.debug(DGTPurposeCacheService.name, 'Starting to get exchange', { uri });
+
+        if (!uri) {
+            throw new DGTErrorArgument('Argument uri should be set.', uri);
+        }
+
+        return this.cache.get<DGTPurpose>(this.transformer, uri);
+    }
+
+    public query(filter?: DGTLDFilter): Observable<DGTPurpose[]> {
+        this.logger.debug(DGTPurposeCacheService.name, 'Starting to query exchanges', filter);
+
+        return this.cache.query(this.transformer, filter);
+    }
+
+    public save(resource: DGTPurpose): Observable<DGTPurpose> {
+        this.logger.debug(DGTPurposeCacheService.name, 'Starting to save resource', { resource });
+
+        if (!resource) {
+            throw new DGTErrorArgument('Argument resource should be set.', resource);
+        }
+
+        if (!resource.uri) {
+            resource.uri = `http://someuri/exchanges#${v4()}`; // TODO set according to strategy
+        }
+
+        return of({ resource })
+            .pipe(
+                switchMap(data => this.cache.save(this.transformer, [resource])
+                    .pipe(map(resources => _.head(resources)))),
+            );
+    }
+
+    public delete(resource: DGTPurpose): Observable<DGTPurpose> {
+        this.logger.debug(DGTPurposeCacheService.name, 'Starting to delete resource', { resource });
+
+        if (!resource) {
+            throw new DGTErrorArgument('Argument resource should be set.', resource);
+        }
+
+        return of({ resource })
+            .pipe(
+                switchMap(data => this.cache.delete(this.transformer, [data.resource])
+                    .pipe(map(resources => ({ ...data, resources })))),
+                map(data => _.head(data.resources))
+            );
+    }
+
+}
