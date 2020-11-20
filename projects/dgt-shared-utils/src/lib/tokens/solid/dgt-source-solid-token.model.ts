41f78b6a6b5eeb8d7d3645ddd25191c5a289bd6d
Arne Vandoorslaer <arne@digita.ai>
2020-11-20 09:55:25 +0100

    fix: cleaned isExpired up

--- projects/dgt-shared-utils/src/lib/tokens/solid/dgt-source-solid-token.model.ts
+++ projects/dgt-shared-utils/src/lib/tokens/solid/dgt-source-solid-token.model.ts
@@ -65,16 +65,24 @@ export class DGTSourceSolidToken extends JWT {
         // return new DGTSourceSolidToken({ header, payload, key: key.cryptoKey }, { filter: false }); // cannot invoke without new on jwt
     }
 
-    static isExpired(accessToken: string): boolean {
+    private static decodeToken(accessToken: string): any {
+        if (!accessToken) {
+            throw new DGTErrorArgument('AccessToken should be set', accessToken);
+        }
         const base64Url = accessToken.split('.')[1];
         const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        const jsonPayload = decodeURIComponent(atob(base64).split('').map((c) => {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        }).join(''));
-        const json = JSON.parse(jsonPayload);
+        const split = atob(base64).split('').map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)).join('');
+        const jsonPayload = decodeURIComponent(split);
+        return JSON.parse(jsonPayload);
+    }
+
+    static isExpired(accessToken: string): boolean {
+        const json = this.decodeToken(accessToken);
+        let expired = false;
         if (json.exp) {
             const unixNow = Math.round((new Date()).getTime() / 1000);
-            return json.exp < unixNow;
-        } else { return false; }
+            expired = json.exp < unixNow;
+        }
+        return expired;
     }
 }
