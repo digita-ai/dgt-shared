5a262af4e5bca3c8c9525f940d9460857cbb05a5
Wouter Janssens <wouter@digita.ai>
2020-11-26 11:34:09 +0100

    fix: fix absolute path in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./node_modules/.bin/ng build dgt-shared-test
-./node_modules/.bin/ng build dgt-shared-utils
-./node_modules/.bin/ng build dgt-shared-data
-./node_modules/.bin/ng build dgt-shared-web
-./node_modules/.bin/ng build dgt-shared-connectors
+../node_modules/.bin/ng build dgt-shared-test
+../node_modules/.bin/ng build dgt-shared-utils
+../node_modules/.bin/ng build dgt-shared-data
+../node_modules/.bin/ng build dgt-shared-web
+../node_modules/.bin/ng build dgt-shared-connectors
