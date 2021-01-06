a1061e7e58cf8486b5a17cabc5b2af0060f35358
Stijn Taelemans <stijn@digita.ai>
2021-01-06 16:03:51 +0100

    feat: updated workflow model

--- projects/dgt-shared-data/src/lib/workflow/models/dgt-workflow.model.ts
+++ projects/dgt-shared-data/src/lib/workflow/models/dgt-workflow.model.ts
@@ -3,6 +3,9 @@ import { DGTLDResource } from '../../linked-data/models/dgt-ld-resource.model';
 import { DGTWorkflowAction } from './dgt-workflow-action.model';
 
 export interface DGTWorkflow extends DGTLDResource {
+    description: string;
+    label: string;
+    icon: string;
     actions: DGTWorkflowAction[];
     filter: DGTLDFilter;
     source: string;
