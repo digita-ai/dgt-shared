daca2562c110ccc78c94adf96bb40d7b5db4ab2c
Arthur Joppart <arthur@digita.ai>
2021-01-13 10:25:10 +0100

    fix: moved logic to LDUtils

--- projects/dgt-shared-data/src/lib/linked-data/services/dgt-ld-utils.service.ts
+++ projects/dgt-shared-data/src/lib/linked-data/services/dgt-ld-utils.service.ts
@@ -1,4 +1,5 @@
-import { DGTInjectable, DGTLoggerService } from '@digita-ai/dgt-shared-utils';
+import { DGTLDResource, DGTLDTriple } from '@digita-ai/dgt-shared-data/public-api';
+import { DGTInjectable, DGTLoggerService, DGTErrorArgument } from '@digita-ai/dgt-shared-utils';
 
 @DGTInjectable()
 export class DGTLDUtils {
@@ -20,5 +21,33 @@ export class DGTLDUtils {
         }
 
         return res;
+    }
+
+    public filterTriplesByPredicates(triples: DGTLDTriple[], predicates: string[]): DGTLDTriple[] {
+        if (!triples) {
+            throw new DGTErrorArgument('Parameter triples should be set', triples);
+        }
+        if (!predicates) {
+            throw new DGTErrorArgument('Parameter predicates should be set', predicates);
+        }
+
+        return triples.filter(tr => predicates.find(pred => tr.predicate === pred)?.length > 0);
+    }
+
+    public filterResourceByPredicates<T extends DGTLDResource>(resource: T, predicates: string[]): T {
+        if (!resource) {
+            throw new DGTErrorArgument('Parameter resource should be set', resource);
+        }
+        if (!predicates) {
+            throw new DGTErrorArgument('Parameter predicates should be set', predicates);
+        }
+        if (!resource.triples) {
+            throw new DGTErrorArgument('Parameter resource.triples should be set', resource.triples);
+        }
+
+        return {
+            ...resource,
+            triples: this.filterTriplesByPredicates(resource.triples, predicates),
+        } as T;
     }
 }
