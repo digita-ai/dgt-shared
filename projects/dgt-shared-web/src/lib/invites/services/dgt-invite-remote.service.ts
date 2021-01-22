298e2e137b387abf2ab01fcef27aa8da7191abf4
Arthur Joppart <arthur@digita.ai>
2021-01-22 11:10:32 +0100

    feat: implemented invite remote service

--- projects/dgt-shared-web/src/lib/invites/services/dgt-invite-remote.service.ts
+++ projects/dgt-shared-web/src/lib/invites/services/dgt-invite-remote.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { DGTInvite, DGTInviteService, DGTLDFilter } from '@digita-ai/dgt-shared-data';
+import { DGTInvite, DGTInviteService, DGTLDFilter, DGTLDFilterService } from '@digita-ai/dgt-shared-data';
 import { DGTConfigurationBaseWeb, DGTConfigurationService, DGTErrorArgument, DGTHttpService, DGTLoggerService } from '@digita-ai/dgt-shared-utils';
-import { Observable, of } from 'rxjs';
+import { Observable, of, forkJoin } from 'rxjs';
 import { map, switchMap, tap } from 'rxjs/operators';
 import { DGTBaseAppState } from '../../state/models/dgt-base-app-state.model';
 import { DGTBaseRootState } from '../../state/models/dgt-base-root-state.model';
@@ -14,6 +14,7 @@ export class DGTInviteRemoteService extends DGTInviteService {
     private logger: DGTLoggerService,
     private config: DGTConfigurationService<DGTConfigurationBaseWeb>,
     public store: DGTStateStoreService<DGTBaseRootState<DGTBaseAppState>>,
+    private filters: DGTLDFilterService,
   ) {
     super();
   }
@@ -32,13 +33,41 @@ export class DGTInviteRemoteService extends DGTInviteService {
       );
   }
   query(filter?: DGTLDFilter): Observable<DGTInvite[]> {
-    throw new Error('Method not implemented.');
+    return of({ filter }).pipe(
+      map(data => ({ ...data, uri: `${this.config.get(c => c.server.uri)}invite/` })),
+      switchMap(data => this.store.select(state => state.app.accessToken).pipe(map(accessToken => ({ ...data, accessToken })))),
+      switchMap(data => this.http.get<DGTInvite[]>(data.uri, { Authorization: `Bearer ${data.accessToken}` })
+        .pipe(map(response => ({ ...data, response})))),
+      switchMap(data => data.filter ? this.filters.run<DGTInvite>(data.filter, data.response.data) : of(data.response.data)),
+    );
   }
   save(resources: DGTInvite[]): Observable<DGTInvite[]> {
-    throw new Error('Method not implemented.');
+    this.logger.debug(DGTInviteRemoteService.name, 'Starting to save', { resources });
+
+    if (!resources) {
+        throw new DGTErrorArgument('Argument resources should be set.', resources);
+    }
+
+    return of({ resources }).pipe(
+      map(data => ({ ...data, uri: `${this.config.get(c => c.server.uri)}invite` })),
+      switchMap(data => this.store.select(state => state.app.accessToken).pipe(map(accessToken => ({ ...data, accessToken })))),
+      switchMap(data => forkJoin(resources.map(resource => this.http.post<DGTInvite>(data.uri, resource, { Authorization: `Bearer ${data.accessToken}` })
+          .pipe(map(response => response.data))))),
+    );
   }
   delete(resource: DGTInvite): Observable<DGTInvite> {
-    throw new Error('Method not implemented.');
+    this.logger.debug(DGTInviteRemoteService.name, 'Starting to delete', { resource });
+
+    if (!resource) {
+        throw new DGTErrorArgument('Argument resource should be set.', resource);
+    }
+
+    return of({ resource }).pipe(
+      map(data => ({ ...data, uri: `${this.config.get(c => c.server.uri)}invite/${encodeURIComponent(data.resource.uri)}` })),
+      switchMap(data => this.store.select(state => state.app.accessToken).pipe(map(accessToken => ({ ...data, accessToken })))),
+      switchMap(data => this.http.delete<DGTInvite>(data.uri, { Authorization: `Bearer ${data.accessToken}` })),
+      map(response => response.data),
+    );
   }
 
   public verify(inviteId: string): Observable<DGTInvite> {
