bbb18f734b978e4626d7880d26ea3ac64830d85c
Wouter Janssens <wouter@digita.ai>
2021-01-23 14:17:52 +0100

    fix: don't load exchanges when generating solid uri

--- projects/dgt-shared-data/src/lib/uri/services/dgt-uri-factory-solid.service.ts
+++ projects/dgt-shared-data/src/lib/uri/services/dgt-uri-factory-solid.service.ts
@@ -1,11 +1,10 @@
-import { DGTErrorArgument, DGTInjectable } from '@digita-ai/dgt-shared-utils';
+import { DGTErrorArgument, DGTInjectable, DGTLoggerService } from '@digita-ai/dgt-shared-utils';
 import { forkJoin, of } from 'rxjs';
 import { Observable } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 import { v4 } from 'uuid';
 import { DGTConnectionSolidConfiguration } from '../../connection/models/dgt-connection-solid-configuration.model';
 import { DGTConnection } from '../../connection/models/dgt-connection.model';
-import { DGTExchangeService } from '../../exchanges/services/dgt-exchange.service';
 import { DGTLDResource } from '../../linked-data/models/dgt-ld-resource.model';
 import { DGTUriFactoryService } from './dgt-uri-factory.service';
 /**
@@ -15,37 +14,46 @@ import { DGTUriFactoryService } from './dgt-uri-factory.service';
  */
 @DGTInjectable()
 export class DGTUriFactorySolidService implements DGTUriFactoryService {
-    constructor(private exchanges: DGTExchangeService) {}
+    constructor(private logger: DGTLoggerService) {}
 
     /**
      * Generates a URI for a resource
      * @param resource The DGTLDResource to generate a uri for
      */
-    public generate(resources: DGTLDResource[], prefix: string, connection: DGTConnection<any>): Observable<DGTLDResource[]> {
+    public generate(
+        resources: DGTLDResource[],
+        prefix: string,
+        connection: DGTConnection<any>,
+    ): Observable<DGTLDResource[]> {
+        this.logger.debug(DGTUriFactorySolidService.name, 'Generating uri', { resources, prefix, connection });
+
         if (!resources) {
             throw new DGTErrorArgument('Argument resources should be set.', resources);
         }
 
         return of({ resources, prefix }).pipe(
             switchMap((data) =>
-                data.resources.length === 0 ? of([]) : forkJoin(
-                    data.resources.map((resource) => 
-                        resource.uri ? of(resource) : this.generateOne(resource, data.prefix,connection),
-                    ),
-                ),
+                data.resources.length === 0
+                    ? of([])
+                    : forkJoin(
+                          data.resources.map((resource) =>
+                              resource.uri ? of(resource) : this.generateOne(resource, data.prefix, connection),
+                          ),
+                      ),
             ),
         );
     }
 
-    private generateOne(resource: DGTLDResource, prefix: string, connection: DGTConnection<any>): Observable<DGTLDResource> {
+    private generateOne(
+        resource: DGTLDResource,
+        prefix: string,
+        connection: DGTConnection<any>,
+    ): Observable<DGTLDResource> {
         if (!resource) {
             throw new DGTErrorArgument('Argument resource should be set.', resource);
         }
 
-        return of({ resource, prefix,connection }).pipe(
-            switchMap((data) =>
-                this.exchanges.get(data.resource.exchange).pipe(map((exchange) => ({ ...data, exchange }))),
-            ),
+        return of({ resource, prefix, connection }).pipe(
             map((data) => ({
                 ...data,
                 typeRegistration: (data.connection
