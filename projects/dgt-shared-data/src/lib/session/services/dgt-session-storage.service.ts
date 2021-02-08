f067e73015d5ae3d3e866f698bc13a19aa01a32d
Wouter Janssens <wouter@digita.ai>
2021-02-08 20:39:24 +0100

    refactor: added logging to session storage

--- projects/dgt-shared-data/src/lib/session/services/dgt-session-storage.service.ts
+++ projects/dgt-shared-data/src/lib/session/services/dgt-session-storage.service.ts
@@ -1,4 +1,4 @@
-import { DGTErrorArgument, DGTInjectable } from '@digita-ai/dgt-shared-utils';
+import { DGTErrorArgument, DGTInjectable, DGTLoggerService } from '@digita-ai/dgt-shared-utils';
 import { IStorage } from '@inrupt/solid-client-authn-core';
 import { EventEmitter } from 'events';
 import { map, tap } from 'rxjs/operators';
@@ -8,42 +8,75 @@ import { DGTConnectionService } from '../../connection/services/dgt-connection-a
 @DGTInjectable()
 export class DGTSessionStorageService extends EventEmitter implements IStorage {
     public get(sessionId: string): Promise<string> {
-        return this.connections.getConnectionBySessionId(sessionId).pipe(
-            map(connection => connection ? JSON.stringify(connection.configuration.sessionInfo) : null),
-        ).toPromise();
+        this.logger.debug(DGTSessionStorageService.name, 'Getting session', { sessionId });
+
+        if (!sessionId) {
+            throw new DGTErrorArgument('Argument sessionId should be set.', sessionId);
+        }
+
+        return this.connections
+            .getConnectionBySessionId(sessionId)
+            .pipe(map((connection) => (connection ? JSON.stringify(connection.configuration.sessionInfo) : null)))
+            .toPromise();
     }
     public set(sessionId: string, value: string): Promise<void> {
+        this.logger.debug(DGTSessionStorageService.name, 'Setting session', { sessionId, value });
+
+        if (!sessionId) {
+            throw new DGTErrorArgument('Argument sessionId should be set.', sessionId);
+        }
+
+        if (!value) {
+            throw new DGTErrorArgument('Argument value should be set.', value);
+        }
+
         const sessionInfo = JSON.parse(value);
 
         const regexMatchedWebId: string[] = sessionInfo.webId.match('(https?://)(.*?/)(.*)');
         const accountId = regexMatchedWebId[2].replace('/', '');
         const protocol = regexMatchedWebId[1];
 
-        return this.connections.getConnectionBySessionId(sessionId).pipe(
-            tap(connection => { if (!connection) { throw new DGTErrorArgument('connection not found', { connection, sessionId }); } }),
-            map(connection => ({
-                ...connection, configuration: {
-                    ...connection.configuration,
-                    accountId,
-                    protocol,
-                    sessionInfo,
-                }, state: sessionInfo.isLoggedIn ? DGTConnectionState.CONNECTED : DGTConnectionState.NOTCONNECTED
-            })),
-            map(connection => this.connections.save([connection]).subscribe()),
-            map(() => { })
-        ).toPromise();
+        return this.connections
+            .getConnectionBySessionId(sessionId)
+            .pipe(
+                tap((connection) => {
+                    if (!connection) {
+                        throw new DGTErrorArgument('connection not found', { connection, sessionId });
+                    }
+                }),
+                map((connection) => ({
+                    ...connection,
+                    configuration: {
+                        ...connection.configuration,
+                        accountId,
+                        protocol,
+                        sessionInfo,
+                    },
+                    state: sessionInfo.isLoggedIn ? DGTConnectionState.CONNECTED : DGTConnectionState.NOTCONNECTED,
+                })),
+                map((connection) => this.connections.save([connection]).subscribe()),
+                map(() => {}),
+            )
+            .toPromise();
     }
 
     public delete(sessionId: string): Promise<void> {
-        return this.connections.getConnectionBySessionId(sessionId).pipe(
-            map(connection => this.connections.delete(connection)),
-            map(() => { })
-        ).toPromise();
+        this.logger.debug(DGTSessionStorageService.name, 'Deleting session', { sessionId });
+
+        if (!sessionId) {
+            throw new DGTErrorArgument('Argument sessionId should be set.', sessionId);
+        }
+
+        return this.connections
+            .getConnectionBySessionId(sessionId)
+            .pipe(
+                map((connection) => this.connections.delete(connection)),
+                map(() => {}),
+            )
+            .toPromise();
     }
 
-    constructor(
-        private connections: DGTConnectionService
-    ) {
+    constructor(private connections: DGTConnectionService, private logger: DGTLoggerService) {
         super();
     }
 }
