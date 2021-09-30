380db31be21312a9bf020b885e84f812c13a147e
Wouter Termont <woutermont@gmail.com>
2021-09-30 19:10:46 +0200

    chore: make formevent interfaces into classes
    Signed-off-by: Wouter Termont <woutermont@gmail.com>

---
+++ packages/dgt-components/lib/forms/form.events.spec.ts
@@ -0,0 +1,40 @@
+import { FormUpdatedEvent, FormSubmittedEvent, FormValidatedEvent } from './form.events';
+import { FormValidatorResult } from './form-validator-result';
+
+describe('FormUpdatedEvent', () => {
+
+  it('should initialize correctly', () => {
+
+    const event = new FormUpdatedEvent('field', 'value');
+    expect(event).toBeTruthy();
+
+  });
+
+});
+
+describe('FormSubmittedEvent', () => {
+
+  it('should initialize correctly', () => {
+
+    const event = new FormSubmittedEvent();
+    expect(event).toBeTruthy();
+
+  });
+
+});
+
+describe('FormValidatedEvent', () => {
+
+  it('should initialize correctly', () => {
+
+    const results: FormValidatorResult[] = [
+      { field: 'field1', message: 'msg1' },
+      { field: 'field2', message: 'msg2' },
+    ];
+
+    const event = new FormValidatedEvent(results);
+    expect(event).toBeTruthy();
+
+  });
+
+});
