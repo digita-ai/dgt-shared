ca1eca2235ab61b121e811224644daef49914068
Abel Van den Briel <43853000+AbelVandenBriel@users.noreply.github.com>
2021-10-21 16:11:06 +0200

    feat: select issuer if only one is found in pod (#89)

--- packages/dgt-components/demo/demo-authenticate.component.ts
+++ packages/dgt-components/demo/demo-authenticate.component.ts
@@ -7,7 +7,7 @@ import { hydrate } from '../lib/util/hydrate';
 
 export class DemoAuthenticateComponent extends RxLitElement {
 
-  private solidService = new SolidSDKService({ clientName: 'UI Transfer' });
+  private solidService = new SolidSDKService({ clientName: 'DemoAuthenticateComponent'});
 
   onAuthenticated = (): void => {  };
   onCreateWebId = (): void => { alert('This is a demo') };
