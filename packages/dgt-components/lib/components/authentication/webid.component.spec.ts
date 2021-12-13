9929e8c10eccd933f742da994656a81fc4f8ac6b
Stijn Taelemans <stijn@digita.ai>
2021-12-13 11:57:29 +0100

    test: update tests

--- packages/dgt-components/lib/components/authentication/webid.component.spec.ts
+++ packages/dgt-components/lib/components/authentication/webid.component.spec.ts
@@ -1,10 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { WebIdComponent } from './webid.component';
 
 describe('WebIdComponent', () => {
 
   let component: WebIdComponent;
+  const webId = 'https://web.id/';
+
+  let input: HTMLInputElement;
+  let form: HTMLFormElement;
 
   beforeEach(() => {
+
+    input = document.createElement('input');
+    input.value = webId;
+    input.name = 'webid';
+
+    form =  document.createElement('form');
+    form.appendChild(input);
 
     component = window.document.createElement('webid-form') as WebIdComponent;
 
@@ -19,6 +31,50 @@ describe('WebIdComponent', () => {
   it('should create', async () => {
 
     expect(component).toBeTruthy();
+
+  });
+
+  describe('onWebIdChange', () => {
+
+    it('should dispatch change-webid CustomEvent', async () => {
+
+      component.dispatchEvent = jest.fn();
+
+      component.onWebIdChange({
+        preventDefault: jest.fn(),
+        target: input,
+      } as any);
+
+      expect(component.dispatchEvent).toHaveBeenCalledWith(expect.any(CustomEvent));
+
+      expect(component.dispatchEvent).toHaveBeenCalledWith(expect.objectContaining({
+        type: 'change-webid',
+        detail: webId,
+      }));
+
+    });
+
+  });
+
+  describe('onSubmit', () => {
+
+    it('should dispatch submit-webid CustomEvent', async () => {
+
+      component.dispatchEvent = jest.fn();
+
+      component.onSubmit({
+        preventDefault: jest.fn(),
+        target: form,
+      } as any);
+
+      expect(component.dispatchEvent).toHaveBeenCalledWith(expect.any(CustomEvent));
+
+      expect(component.dispatchEvent).toHaveBeenCalledWith(expect.objectContaining({
+        type: 'submit-webid',
+        detail: webId,
+      }));
+
+    });
 
   });
 
