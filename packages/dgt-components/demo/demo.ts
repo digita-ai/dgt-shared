c99a2470956df0a240f6ffd780a29e65274a3806
JasperVandenberghen <jasper.vandenberghen@gmail.com>
2022-01-06 19:13:44 +0100

    chore: deleted replaced components from demo and index file

--- packages/dgt-components/demo/demo.ts
+++ packages/dgt-components/demo/demo.ts
@@ -1,28 +1,18 @@
 import { Parser, Store } from 'n3';
 import { addListener, ComponentEventTypes, ComponentReadEvent, ComponentResponseEvent, ComponentWriteEvent } from '@digita-ai/semcom-sdk';
-import {ProfileNameComponent} from '../lib/components/profile/profile-name.component';
 import {FormElementComponent} from '../lib/components/forms/form-element.component';
 import {CardComponent} from '../lib/components/cards/card.component';
-import { ProfileContactComponent } from '../lib/components/profile/profile-contact.component';
-import { ProfilePayslipComponent } from '../lib/components/profile/profile-payslip.component';
 import { DemoAuthenticateComponent } from './demo-authenticate.component';
 import { ListItemComponent } from '../lib/components/list-item/list-item.component';
 import { DemoComponent } from './demo.component';
-import { BarcodeComponent } from '../lib/components/barcode/barcode.component';
-import { DocumentComponent } from '../lib/components/document/document.component';
 
 
 
 customElements.define('demo-auth', DemoAuthenticateComponent);
 customElements.define('nde-form-element', FormElementComponent);
 customElements.define('nde-card', CardComponent);
-customElements.define('profile-name-component', ProfileNameComponent);
-customElements.define('profile-contact-component', ProfileContactComponent);
-customElements.define('profile-payslip-component',  ProfilePayslipComponent);
 customElements.define('list-item', ListItemComponent);
 customElements.define('demo-component', DemoComponent);
-customElements.define('barcode-component', BarcodeComponent);
-customElements.define('document-component', DocumentComponent);
 
 const parser = new Parser();
 
