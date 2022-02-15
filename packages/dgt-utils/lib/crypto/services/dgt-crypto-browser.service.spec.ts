5a4a7442123ff1df98a3a63fbc89688ffbae3bce
Arthur Joppart <arthur@digita.ai>
2022-02-15 10:25:08 +0100

    fix: linting

--- packages/dgt-utils/lib/crypto/services/dgt-crypto-browser.service.spec.ts
+++ packages/dgt-utils/lib/crypto/services/dgt-crypto-browser.service.spec.ts
@@ -1,12 +1,13 @@
 /**
  * @jest-environment node
  */
-import { DGTCryptoBrowserService } from './dgt-crypto-browser.service';
 import { DGTLoggerService } from '../../logging/services/dgt-logger.service';
+import { DGTCryptoBrowserService } from './dgt-crypto-browser.service';
 
 describe('DGTCryptoBrowserService', () => {
 
   let service: DGTCryptoBrowserService;
+
   const mockLogger: DGTLoggerService = {
     debug: jest.fn(),
   } as any;
