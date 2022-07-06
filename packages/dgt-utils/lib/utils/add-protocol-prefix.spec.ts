70d9aa4540704ff18407cdadf6a61e2a2c6472c2
LiamThys <liam.thys11@gmail.com>
2022-07-06 15:17:30 +0200

    chore: refactor addprotocolprefix tests

--- packages/dgt-utils/lib/utils/add-protocol-prefix.spec.ts
+++ packages/dgt-utils/lib/utils/add-protocol-prefix.spec.ts
@@ -1,28 +1,46 @@
+import fetchMock from 'jest-fetch-mock';
 import { addProtocolPrefix } from './add-protocol-prefix';
+
+const MOCK_URI = 'example.com';
+
+fetchMock.enableMocks();
+
+beforeEach(() => {
+
+  fetchMock.resetMocks();
+
+});
 
 describe('addProtocolPrefix()', () => {
 
   it('should return uri with https when https is working', async () => {
 
-    const uri = 'example.com';
-    const response = addProtocolPrefix(uri);
-    await expect(response).resolves.toBe('https://' + uri);
+    fetchMock.mockResponseOnce(JSON.stringify({ uri: 'https://' + MOCK_URI }));
+
+    const response = addProtocolPrefix(MOCK_URI);
+
+    await expect(response).resolves.toBe('https://' + MOCK_URI);
 
   });
 
   it('should return uri with http when https is not working', async () => {
 
-    const uri = 'info.cern.ch';
-    const response = addProtocolPrefix(uri);
-    await expect(response).resolves.toBe('http://' + uri);
+    fetchMock.mockRejectOnce();
+    fetchMock.mockResponseOnce(JSON.stringify({ uri: 'http://' + MOCK_URI }));
+
+    const response = addProtocolPrefix(MOCK_URI);
+
+    await expect(response).resolves.toBe('http://' + MOCK_URI);
 
   });
 
   it('should throw when uri cannot be fetched', async () => {
 
-    const uri = 'unknownuri.org';
-    const response = addProtocolPrefix(uri);
-    await expect(response).rejects.toThrowError(`Could not add protocol prefix to ${uri}`);
+    fetchMock.mockReject();
+
+    const response = addProtocolPrefix(MOCK_URI);
+
+    await expect(response).rejects.toThrow(`Could not add protocol prefix to ${MOCK_URI}`);
 
   });
 
--- packages/dgt-utils/lib/utils/add-protocol-prefix.ts
+++ packages/dgt-utils/lib/utils/add-protocol-prefix.ts
@@ -1,5 +1,3 @@
-import fetch from 'cross-fetch';
-
 /**
  *
  * Function to add protocol prefix to uri if it is missing
